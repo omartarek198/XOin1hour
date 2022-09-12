ca356b2d21ff27e9210527aa3812dc242c4ea8e4
Omar <omar.tarek29@msa.edu.eg>
2022-09-12 10:19:59 +0200

    added validation + placement

--- index.js
+++ index.js
@@ -1,60 +1,88 @@
-class XO {
-    board = [3][3];
-    currSymbol = 'O'
+const { get } = require("http");
 
+class XO {
+  board = [];
+  currSymbol = 'O';
+
+
+  constructor()
+  {
+    this.board = []
+    for (var i = 0; i < 3; i++) {
+      this.board[i] = [];
+    }
+
+    for (var i = 0; i < 3; i++)
+    {
+      for (var k = 0; k < 3; k++)
+      {
+          this.board[i][k] = ' '
+        }
+      }
+  }
   displayBoard() {
     var cell = 0;
     for (var i = 0; i < 3; i++) {
       for (var j = 0; j < 3; j++) {
         // console.log("|  ", cell, "   |")
+        if (this.board[i][j] == ' ')
+        {
         process.stdout.write(`${"|  " + cell + "   |"}`);
+          
+        }
+        else
+        {
+        process.stdout.write(`${"|  " + this.board[i][j] + "   |"}`);
+          
+          }
         cell++;
       }
       console.log("");
     }
   }
-    placeXOinCell(cell)
-    {
-        if (cell > 8 || cell < 0)
-            return -1
-        
-       const rows = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-        var ct = 0
-        var R = 0
-        var C = 0
-        for (var i = 0; i < 3;i++)
-        {
+  placeXOinCell(cell) {
+    if (cell > 8 || cell < 0) return -1;
 
-            var row = rows[i]
-            var index = row.indexOf(cell)
-                // console.log(index);
-                // console.log(row)
-            if (index > -1)
-            {
-                C = ct;
-                R = index;
-                break;
-            }
-            ct++;
-        }
-
-        // console.log("R: ", R)
-        // console.log("C: ", C)
-
-        if (this.board[R][C] != ' ')
-            return -1
-        else
-        {
-            this.board[R][C] = currSymbol;    
-        }
+    const rows = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+    ];
+    var ct = 0;
+    var R = 0;
+    var C = 0;
+    for (var i = 0; i < 3; i++) {
+      var row = rows[i];
+      var index = row.indexOf(cell);
+      // console.log(index);
+      // console.log(row)
+      if (index > -1) {
+        C = ct;
+        R = index;
+        break;
+      }
+      ct++;
     }
+
+    // console.log("R: ", R)
+    // console.log("C: ", C)
+
+    if (this.board[R][C] != " ") return -1;
+    else {
+      this.board[R][C] = this.currSymbol;
+      return 1;
+    }
+
+
+    return 0;
+  }
   validate(x) {
     var num = parseInt(x);
     if (Number.isNaN(num)) {
       console.log("invalid");
       return -1;
     } else {
-        return num
+      return num;
     }
   }
   getCellFromUser() {
@@ -62,18 +90,28 @@ class XO {
       input: process.stdin,
       output: process.stdout,
     });
-     var cell = 0
+    var cell = 0;
     readline.question("Enter Cell Number", (cell) => {
-        cell = this.validate(cell);
-         if (cell == -1) {
-           this.getCellFromUser();
-         } else {
-           this.placeXOinCell(cell);
-         }
+      cell = this.validate(cell);
+      if (cell == -1) {
+        this.getCellFromUser();
+      } else {
+        var result = 0
+        var result = this.placeXOinCell(cell);
+        if (result == -1) {
+          console.log("invalid");
+          this.getCellFromUser();
+        }
+        if (result == 1) {
+          this.displayBoard();
+          this.currSymbol == "O"
+            ? (this.currSymbo = "X")
+            : (this.currSymbol = "O");
+          this.getCellFromUser();
+        }
+      }
       readline.close();
     });
-     
-      
   }
 }
 
